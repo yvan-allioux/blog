3056896c32fba5c9536642c16840f8b1ded4ac28
yvan-allioux <yvan.allioux.pro@gmail.com>
2024-03-11 16:09:01 +0100

    Update build.py

--- build/build.py
+++ build/build.py
@@ -1,20 +1,24 @@
-
 import os
 import markdown
 
 def convert_markdown_to_html(directory):
     for foldername, subfolders, filenames in os.walk(directory):
+        print(f'Processing folder: {foldername}')
         for filename in filenames:
             if filename.endswith('.md'):
                 file_path = os.path.join(foldername, filename)
+                print(f'Processing file: {file_path}')
                 with open(file_path, 'r') as f:
                     text = f.read()
 
                 html = markdown.markdown(text)
 
                 html_file_path = os.path.join(foldername, os.path.splitext(filename)[0] + '.html')
+                print(f'Writing file: {html_file_path}')
                 with open(html_file_path, 'w') as f:
                     f.write(html)
+
+convert_markdown_to_html('articles')
 
 print("start build.py")
 convert_markdown_to_html('articles')
